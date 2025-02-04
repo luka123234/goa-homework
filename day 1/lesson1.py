d721f3d195ebc1577f47f28350681b9b7b7fcc02
luka123234 <lukaroinishvili@gmail.com>
2025-02-04 20:37:15 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,33 @@
+name = "luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშნელობის მიმნიჭებელი სიმბოლო
+# "luka" ცვლადისთვის მნიშვნელობა 
+
+surname = "roinishvili"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "luka" #ეს არის str (string) ტიპის ცვლადი
+age = 11 #ეს არის int (integer) მთელი რიცხვი
+height = 147.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (boal) ტიპის ცვლადი
+
+knows_programming = True  #true ან false
+is_ugly = False  #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False # ჯავასკრიპტული camlecase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოცემული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type surname)
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
