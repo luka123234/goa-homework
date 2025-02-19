2ba0fa453c9a0190311e31b08aced9c33cf72845
luka123234 <lukaroinishvili@gmail.com>
2025-02-19 18:13:59 +0400

    second commit

---
+++ level03/homework.py
@@ -0,0 +1,49 @@
+#str არის ცვლადი რომელშიც სიტყვა ან ნებისმიერი რამ იწერება ბრჭყალებში
+
+name = "luka" #ეს ცვლადის ტიპი არის str
+
+lastname = "roinishvili" #ეს ცვლადის ტიპიც არის str
+
+item = "phone" #ეს ცვლადის ტიპიც არის str
+
+#int არის ნებისმიერი მთელი რიცხვი რომელიც არ იწერება ბრჭყალებში
+
+age = 11
+
+date = 19
+
+month = 2
+
+#float არის ნებისმიერი ათწილადი რიცხვი
+
+weight = 44.8
+
+height = 147.50
+
+lenght = 5.20
+
+
+print(name)
+print(lastname)
+print(item)
+print(age)
+print(date)
+print(month)
+print(weight)
+print(lenght)
+
+print(5+5)
+print(10-5)
+print(49/7)
+print(20 * 5)
+
+
+car = "bmw"
+
+fruit = "apple"
+
+leptop = "lenovo"
+
+consol = "ps4"
+
+print(19 + 2 - 3 / 2 * 3 / 3 * 2 / 2)
