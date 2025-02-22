88a8b534e31e82285256c9d26845da1ce0abe99d
luka123234 <lukaroinishvili@gmail.com>
2025-02-22 10:40:15 +0400

    second commit

---
+++ leve04/classwor.py/homework.py
@@ -0,0 +1,45 @@
+#  variables
+num1 = 0
+num2 = 0
+num3 = 0
+
+# Update the variables 5 times using a loop
+for i in range(5):
+    num1 += 1  # Add 1 to num1 each time
+    num2 += 2  # Add 2 to num2 each time
+    num3 += 3  # Add 3 to num3 each time
+
+# Print the final values
+print(f"num1: {num1}, num2: {num2}, num3: {num3}")
+
+# 1
+num1 = 'one' #აქ არ ჰქონდა ბრჭყალები
+print(num1)
+
+# 2
+num2 = 'two' #აქ ზედმეტი იყო ფრჩხილები
+
+# 3
+Num3 = 'three' #ეს სწორი იყო
+print(Num3)
+
+# 4
+num4 = 'number4' #აქ ერთი სხვანაირი ბრჭყალი იყო და ერთი სხვანაირი
+print(num4)
+
+# 5
+num5 = 'five' #აქ შეცდომა არ იყო
+print(num5)
+
+
+# 4 მთელი ცვლადის შექმნა
+num1 = 10
+num2 = 20
+num3 = 30
+num4 = 40
+
+# მე-5 ცვლადის შექმნა 4 ცვლადის ჯამის შესანახად
+sum_of_nums = num1 + num2 + num3 + num4
+
+# მე-5 ცვლადში შენახული შედეგის დაბეჭდვა
+print(sum_of_nums)
