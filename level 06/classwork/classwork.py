072a9c7a1bfd9d479aca828c92b3a32ca932dc6a
luka123234 <lukaroinishvili@gmail.com>
2025-03-01 10:51:36 +0400

    second commit

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,26 @@
+#case-sensitive არის ზუსტა იგვე რაღაცის გადაწერა რაც საც უკვე გიწერია ცვლადში თუ დიდი ასო გიწერია
+#ცვლადში ცვლადის სახელი PRINTშიც დიდი ასოთი უნდა დაწერო
+
+CAR = "bmw"
+print(CAR)
+
+#snake-case არის როდესაც გამოტოვებას წერ ქვედა ტირეთი
+
+group_62_is_best = "best"
+
+
+
+Name = 'nika'
+print(name)
+
+goa_best = 100
+print(goa best)
+
+name - 'mate'
+print(naMe)
+
+number = 10
+print(number)
+
+goa_best = (1000%)
+print(goaBest)
